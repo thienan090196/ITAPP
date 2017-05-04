eb364c06d670fbeba2c5379ea2f51a3a737a6756
LinhTranPN <linh.tran1@student.passerellesnumeriques.org>
2017-05-04 08:20:52 +0700

    update

--- app/src/main/java/com/example/cuongtran/itapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/cuongtran/itapp/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.cuongtran.itapp.activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +15,23 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-
 import com.example.cuongtran.itapp.R;
 import com.example.cuongtran.itapp.app.AppController;
+import com.example.cuongtran.itapp.model.Account;
+import com.example.cuongtran.itapp.model.AccountBuilder;
+import com.example.cuongtran.itapp.model.Post;
+import com.example.cuongtran.itapp.model.Profile;
 import com.example.cuongtran.itapp.utils.Common;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 public class LoginActivity extends BaseActivity {
@@ -89,13 +97,19 @@ public class LoginActivity extends BaseActivity {
                             //Get api message content
                             //String message = response.getString("message");
                             JSONObject jsonObject = new JSONObject(response);
-                            JSONObject account = jsonObject.getJSONObject("dataObject");
-                            String email = account.getString("email");
-                            String password = account.getString("password");
+
+                            JSONObject dataObject = jsonObject.getJSONObject("dataObject");
+                            String email = dataObject.getString("email");
+                            String password = dataObject.getString("password");
+                          //  String id_user = dataObject.getString("id_user");
                             if (email.equals(edtEmail.getText().toString().trim()) && password.equals(edtPassword.getText().toString().trim())) {
                                 Toast.makeText(LoginActivity.this, "Login Succeeded!", Toast.LENGTH_LONG).show();
+                                Account account = AccountBuilder.getAccount(jsonObject);
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                intent.putExtra("ac", account);
+                                startActivity(intent);
                             }
-                            Toast.makeText(LoginActivity.this, "Your email or password is incorrect!", Toast.LENGTH_LONG).show();
+                            else Toast.makeText(LoginActivity.this, "Your email or password is incorrect!", Toast.LENGTH_LONG).show();
                         } catch (JSONException e) {
                             Toast.makeText(LoginActivity.this, "Your email or password is incorrect!", Toast.LENGTH_LONG).show();
                         }
@@ -123,4 +137,6 @@ public class LoginActivity extends BaseActivity {
                 };
         AppController.getInstance().addToRequestQueue(stringRequest, tag_json_obj);
     }
+
+
 }
---
+++ app/src/main/java/com/example/cuongtran/itapp/model/Comment.java
@@ -0,0 +1,8 @@
+package com.example.cuongtran.itapp.model;
+
+/**
+ * Created by linh.tran1 on 03/05/2017.
+ */
+
+public class Comment {
+}
